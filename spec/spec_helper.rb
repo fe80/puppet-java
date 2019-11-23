9a7726016ec433dbfe9df40c24c21e500c439061
fe80 <steffyfort@gmail.com>
2019-11-23 15:49:49 +0100

    fix spec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -44,4 +44,9 @@ def ensure_module_defined(module_name)
   end
 end
 
+RSpec.configure do |c|
+  c.after(:suite) do
+    RSpec::Puppet::Coverage.report!
+  end
+end
 # 'spec_overrides' from sync.yml will appear below this line
